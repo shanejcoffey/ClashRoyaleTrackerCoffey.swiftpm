cb212edccf026d76be6bed675c4af5934c4081cb
Shane Coffey <shanejcoffey27@gmail.com>
2025-12-05 10:36:33 -0600

    Update Deck.swift

--- Deck.swift
+++ Deck.swift
@@ -6,23 +6,34 @@
 //
 
 import SwiftUI
+import SwiftData
 
+@Model
 class Deck: Identifiable, ObservableObject {
-    @Published var cards: [Card?]
+    @Relationship var cards: [Card]
     
-    init(cards: [Card?]) {
+    init(cards: [Card]) {
         self.cards = cards
     }
     
     init() {
-        cards = [nil, nil, nil, nil, nil, nil, nil, nil]
+        cards = [Card(), Card(), Card(), Card(), Card(), Card(), Card(), Card()]
     }
     
-    func setCard (card: Card?, index: Int) -> Card? {
-        let replacedCard: Card? = cards[index]
+    func setCard (card: Card, index: Int) -> Card {
+        let replacedCard: Card = cards[index]
         var newCards = cards
         newCards[index] = card
         cards = newCards
         return replacedCard
+    }
+    
+    func isFull() -> Bool {
+        for card in cards {
+            if card.name == "" {
+                return false
+            }
+        }
+        return true
     }
 }
