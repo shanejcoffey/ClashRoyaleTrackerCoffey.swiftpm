dbeb6b5faf4edc537206016f040b9075373e0ae4
Shane Coffey <shanejcoffey27@gmail.com>
2025-12-05 10:38:06 -0600

    Update MyApp.swift

--- MyApp.swift
+++ MyApp.swift
@@ -1,10 +1,12 @@
 import SwiftUI
+import SwiftData
 
 @main
 struct MyApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: [Battle.self, Deck.self, Card.self])
         }
     }
 }
